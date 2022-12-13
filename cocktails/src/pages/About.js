3ebe4af92201bb2b03168418b7b9c2db5650bbe0
HossamKhafagy <hossamkhafagy8@gmail.com>
2022-12-13 03:30:50 +0200

    initial commit

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Nulla incididunt sit ipsum ipsum laboris nostrud reprehenderit tempor
+        nulla consequat. Ipsum cupidatat non voluptate aliquip Lorem
+        exercitation magna ipsum do mollit deserunt mollit officia elit. Velit
+        eiusmod in eu labore irure est laborum commodo excepteur enim occaecat
+        aute.Elit sit commodo magna aliquip cupidatat in exercitation eu minim.
+        Excepteur veniam amet aute ex commodo veniam sit consequat nisi officia
+        commodo enim. Irure sit voluptate labore adipisicing magna nisi
+        voluptate. Exercitation aute adipisicing reprehenderit excepteur velit
+        laborum. Sint ea veniam duis nostrud reprehenderit id pariatur anim
+        adipisicing in.
+      </p>
+    </section>
+  );
+};
+
+export default About;
